c81a32c7f84ca931392a0bb11ea167218204c3dc
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-06-19 22:47:37 +0200

    add files, changes

---
+++ index.js
@@ -0,0 +1,21 @@
+process.stdin.setEncoding('Utf-8');
+var fs = require('fs');
+
+fs.readdir('./.git', 'utf-8', function(err, files){
+	if(err) throw err;
+	var directoryContent = files;
+		
+	for(var i=0; i<directoryContent.length; i++){
+		var position = '\n' + directoryContent[i];
+		console.log(position);
+		fs.appendFile('./gitdirectory1.txt', position , function(err){
+			if(err) throw err;
+			});
+	}
+	console.log('\nWriting the file done');
+});
+
+fs.writeFile('./gitdirectory1.txt', '', function(err, data){
+	if(err) throw err;
+	console.log('\nClearing the file done');
+});
---
+++ modules/time.js
@@ -0,0 +1,12 @@
+var os = require('os');
+process.stdin.setEncoding('utf8');
+
+function getUptime(){
+  var uptime = os.uptime();
+  var uptimeRounded = Math.round(uptime);
+  var uptimeMinutes = Math.floor(uptimeRounded/60);
+  var uptimeSeconds = uptimeRounded-(uptimeMinutes*60);
+  console.log('Uptime: ' +  uptimeMinutes + " min " + uptimeSeconds + " sec");
+}
+
+exports.getUptime = getUptime;
